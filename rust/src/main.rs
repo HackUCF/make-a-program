cc198297e2429471d76c8c4417817f0f55254775
Roux <pupoi@tutanota.com>
2023-04-14 10:44:59 -0400

    rust: gitignore and initialize rust project

---
+++ rust/src/main.rs
@@ -0,0 +1,12 @@
+
+fn main() {
+    println!("Hello :D");
+    println!("This program adds 4 and 5!");
+    res = add_nums(5, 4);
+    println!("The result is {}!", res)
+}
+
+fn add_nums(int i, int j) {
+    i + j
+}
+
